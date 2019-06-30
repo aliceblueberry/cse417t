01f8afa51b9d9254d7f3b2bd3c019a50920b866a
Rachana Reddy <rachanaereddy@gmail.com>
2019-06-30 13:28:55 -0500

    rachana's edits

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-//connects to users database
-$mysqli = new mysqli('localhost', 'rachanaereddy', 'warren99', 'databasename');
- 
-if($mysqli->connect_errno) {
-	printf("Connection Failed: %s\n", $mysqli->connect_error);
-	exit;
-}
-?>
